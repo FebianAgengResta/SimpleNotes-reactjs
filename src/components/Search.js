c3282c37eb22c226c7de10c65743d2d9dc91b35c
bongs <restaageng@gmail.com>
2023-12-15 12:51:08 +0700

    add notes

---
+++ src/components/Search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { MdSearch } from 'react-icons/md';
+import { IoMdSearch } from "react-icons/io";
+
+const Search = ({ handleSearchNote }) => {
+	return (
+		<div className='search'>
+    		
+			<IoMdSearch className='search-icons' size='1.3em' />
+			<input
+				onChange={(event) =>
+					handleSearchNote(event.target.value)
+				}
+				type='text'
+				placeholder='type to search...'
+			/>
+		</div>
+	);
+};
+
+export default Search;
